aa6d909e29ed8c4d90acc0cef3597a00fd48526a
Laurent Berthier <proberthier@gmail.com>
2015-10-21 12:05:47 +0200

    new contents in postService

--- public/js/controllers/post.js
+++ public/js/controllers/post.js
@@ -1,3 +1,18 @@
 function postController($scope, postService) {
+  var user = localStorage.getItem("user");
+  var dato = {};
+  $scope.user = user;
   
+  $scope.jeclick = function(){
+    console.log($scope.vm.picture);
+    dato.picture = $scope.vm.picture;
+    dato.author = user;
+    postService.post(dato)
+      .success(function(data) {
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error : ' + data);
+      });
+  }
 }
