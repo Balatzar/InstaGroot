c3d93f102ada96d457e3a3948279d34c703896d3
Balatzar <bataprod@gmail.com>
2015-10-29 17:36:30 +0100

    fail

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 
 module.exports = {
-  url : "mongodb://heroku_hngrtmmk:heroku_hngrtmmk@ds047114.mongolab.com:47114/heroku_hngrtmmk"
+  url : 'mongodb://localhost/todoyes'
 };
